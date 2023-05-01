5ba4b3d56b93f26efb151fab8b05ccc16a0a8223
gjuncos <gabbjuncos@gmail.com>
2023-05-01 19:09:53 -0300

    cambios req

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -o errexit
 #poetry install
 pip install -r requirements.txt
 
-pip install --upgrade --no-cache-dir git+https://github.com/StreamAlpha/tvdatafeed.git
-
-pip install TA-Lib
+pip3 install tvdatafeed
 
 python manage.py collectstatic --no-input
 python manage.py migrate
